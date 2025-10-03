b58e50714fc72db157bc981ba5b1949f6cd38da3
Dominic0607 <114179300+Dominic0607@users.noreply.github.com>
2025-10-04 03:00:44 +0700

    Ver.1
    Ver.1

---
+++ config.js
@@ -0,0 +1,10 @@
+// --- Configuration for Frontend (index.html) ---
+
+/**
+ * សំខាន់៖ នេះគឺជា URL សម្រាប់ឲ្យหน้าเว็บទាក់ទងទៅកាន់ Backend។
+ * ១. ចូលទៅកាន់ 'Deploy > Manage deployments'។
+ * ២. ចម្លង Web App URL (Link ដែលបញ្ចប់ដោយ '/exec') ជំនាន់ចុងក្រោយបំផុត។
+ * ៣. យកមកដាក់ជំនួសពាក្យ "PASTE_YOUR_NEW_WEB_APP_EXEC_URL_HERE" ខាងក្រោម។
+ */
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzabT13riuRgmCo4aq4KaCJAxKM9E79dC167FKp4oCU60TGIJJp0L6qlCqxCOqgjd9FzQ/exec";
+
