3905d7b9e1f1507161dec051d64e789e613e8d31
Dominic0607 <114179300+Dominic0607@users.noreply.github.com>
2025-10-05 21:30:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,8 @@
  * 2. ចូលទៅកាន់ 'Deploy > Manage deployments' នៅក្នុង Apps Script។
  * 3. Deploy ជា "New version" រួចចម្លង (Copy) Web App URL ថ្មី។
  * 4. បិទភ្ជាប់ (Paste) URL ថ្មីនោះនៅខាងក្រោមនេះ ដើម្បីជំនួស URL ចាស់។
+ * **ចំណាំ:** URL នេះត្រូវតែជាប្រភេទ 'Execute as: Me' និង 'Who has access: Anyone, even anonymous'
  */
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxegEuT97cJY53cEBRHXFG89xvmXm6TFkwDc5_APNcgujKt2LiWUoawUtTL50BqbKcl/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyPtAtOsZ3RwasV74Zi9fvCO0NgkGpnSnsTCe-4F4RgjB7ytX8y6He5ifSzyZo5Dj0o/exec";
+
+// ដើម្បីដំណើរការត្រឹមត្រូវជាមួយ Hosting ខាងក្រៅ សូមជំនួស URL ខាងលើដោយ URL របស់ Deployment ចុងក្រោយរបស់អ្នក។
