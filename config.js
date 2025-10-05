423405d917fa891d923ae53feb5349d89b71c100
Dominic0607 <114179300+Dominic0607@users.noreply.github.com>
2025-10-05 15:59:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 // --- Configuration for Frontend (index.html) ---
 
 /**
- * សំខាន់៖ នេះគឺជា URL សម្រាប់ឲ្យหน้าเว็บទាក់ទងទៅកាន់ Backend។
- * ១. ចូលទៅកាន់ 'Deploy > Manage deployments'។
- * ២. ចម្លង Web App URL (Link ដែលបញ្ចប់ដោយ '/exec') ជំនាន់ចុងក្រោយបំផុត។
- * ៣. យកមកដាក់ជំនួសពាក្យ "PASTE_YOUR_NEW_WEB_APP_EXEC_URL_HERE" ខាងក្រោម។
+ * !!! សំខាន់ខ្លាំងណាស់ !!!
+ * នេះគឺជា URL សម្រាប់ Web App របស់អ្នកដើម្បីទាក់ទងជាមួយ Backend (Google Apps Script)។
+ * 1. សូមអនុវត្តតាមការណែនាំនៅក្នុងឯកសារ "troubleshooting.md"។
+ * 2. ចូលទៅកាន់ 'Deploy > Manage deployments' នៅក្នុង Apps Script។
+ * 3. Deploy ជា "New version" រួចចម្លង (Copy) Web App URL ថ្មី។
+ * 4. បិទភ្ជាប់ (Paste) URL ថ្មីនោះនៅខាងក្រោមនេះ ដើម្បីជំនួស URL ចាស់។
  */
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzabT13riuRgmCo4aq4KaCJAxKM9E79dC167FKp4oCU60TGIJJp0L6qlCqxCOqgjd9FzQ/exec";
-
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxegEuT97cJY53cEBRHXFG89xvmXm6TFkwDc5_APNcgujKt2LiWUoawUtTL50BqbKcl/exec";
